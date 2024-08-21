e003a83929e59a3557ea32123d40be08e58b401b
mounika-kota <mounikota9141@gmail.com>
2024-08-21 17:37:47 +0530

    PlanController.java

--- src/main/java/com/overalt/controller/PlanController.java
+++ src/main/java/com/overalt/controller/PlanController.java
@@ -4,16 +4,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.overalt.service.CustomerService;
+import com.overalt.model.Plan;
+import com.overalt.service.PlanService;
 
 @RestController
 public class PlanController {
+
     @GetMapping("/getPlanByid")
-    public String getPlanById(@RequestParam int plan_id) {
+    public Plan getPlanByid(@RequestParam int plan_id) {
+        PlanService planservice = new PlanService(null);
+        return planservice.getPlanById(plan_id);
         
     }
 
-    
-
-    
+    @GetMapping("/getPlanByname")
+    public String getPlanByname(@RequestParam String plan_name) {
+        PlanService planService = new PlanService(null);
+        return planService.getPlanByName(plan_name).toString();
+    }
 }
