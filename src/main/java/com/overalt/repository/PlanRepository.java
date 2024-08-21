dfc6c5c7be8190ce7d682afda54ce534a95862d6
mounika-kota <mounikota9141@gmail.com>
2024-08-21 15:29:20 +0530

    Modified PlanRepository.java

--- src/main/java/com/overalt/repository/PlanRepository.java
+++ src/main/java/com/overalt/repository/PlanRepository.java
@@ -1,8 +1,13 @@
 package com.overalt.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
+import com.overalt.model.Plans;
+
+
 @Service
-public class PlanRepository {
-    
+public interface PlanRepository extends JpaRepository<Plans, Integer> {
+    Plans findByPlan_id(int plan_id);
+    Plans findByPlan_name(String plan_name);
 }
