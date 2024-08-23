0dcae702774357fc4d8239ea6fbff0b3ee0c746a
venkatesh-reddi <venkateshreddi@outlook.com>
2024-08-23 23:33:21 +0530

    Added more CRUD operations to repositories.

--- src/main/java/com/overalt/repository/FriendOrFamilyRepository.java
+++ src/main/java/com/overalt/repository/FriendOrFamilyRepository.java
@@ -1,12 +1,16 @@
 package com.overalt.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.overalt.model.Customer;
 import com.overalt.model.FriendOrFamily;
 
 @Repository
 public interface FriendOrFamilyRepository extends JpaRepository<FriendOrFamily, Long> {
     // Custom method to find FriendOrFamily by contact number
     FriendOrFamily findByContactNumber(long contactNumber);
+    List<FriendOrFamily> findByCustomer(Customer customer);
 }
