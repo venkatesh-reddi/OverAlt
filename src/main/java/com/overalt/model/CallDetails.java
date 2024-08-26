ec2bc6552227739e9f3908b26ae5c2cf9138b84a
sakina-n72 <nsakina72@gmail.com>
2024-08-26 14:31:28 +0530

    bug fixed in post and put in call details

--- src/main/java/com/overalt/model/CallDetails.java
+++ src/main/java/com/overalt/model/CallDetails.java
@@ -5,8 +5,6 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -16,8 +14,9 @@ import jakarta.persistence.Table;
 public class CallDetails {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="call_id")
     private Integer callId;
+    
 
     @Column(name = "caller_id", nullable = false)
     private Long callerId;
