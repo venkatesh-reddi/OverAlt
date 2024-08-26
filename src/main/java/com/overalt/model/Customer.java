812b83071580c279baa833253e8fd5087268b883
venkatesh-reddi <venkateshreddi@outlook.com>
2024-08-26 16:10:55 +0530

    Removed the auto generation of id from customer entity.

--- src/main/java/com/overalt/model/Customer.java
+++ src/main/java/com/overalt/model/Customer.java
@@ -7,8 +7,6 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -21,7 +19,6 @@ import jakarta.persistence.Table;
 public class Customer {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // Assuming customer_id is auto-generated
     private int customerId;
 
     @OneToMany(mappedBy = "customer")
