3b038df5afb75430024bcc7ce523a556bbba9c29
Venkatesh Reddi <90749974+venkatesh-reddi@users.noreply.github.com>
2024-08-27 09:52:02 +0530

    Revert "Exception"

--- src/main/java/com/overalt/Exception/Customer/InvalidCustomerDataException.java
+++
@@ -1,7 +0,0 @@
-package com.overalt.Exception.Customer;
-
-public class InvalidCustomerDataException extends Exception {
-    public InvalidCustomerDataException(String message) {
-        super(message);
-    }
-}
